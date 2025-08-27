01ca04728f3b908790739d42df030ab668da7dd3
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-08-27 14:58:57 +0700

    tes2

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'zahran-musyaffa-footballnews.pbp.cs.ui.ac.id']
 
 
 # Application definition
