0682631b7fac67dc2fdcd1d129d4072261d5f13c
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-09-17 14:15:30 +0700

    tutorial 3 completed 4

--- football_news/settings.py
+++ football_news/settings.py
@@ -154,3 +154,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
