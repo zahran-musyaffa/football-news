896685c6b9c6329e92b778e7b5978200f82eb4c4
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-09-17 14:30:36 +0700

    tes

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import News
 class NewsForm(ModelForm):
     class Meta:
         model = News
-        fields = ["title", "content", "category", "thumbnail", "is_featured"]     
+        fields = ["title", "content", "category", "thumbnail", "is_featured"  ]     
