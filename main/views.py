46d68252566b661f5ef372d7e0752d34203dbaf7
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2025-09-24 13:55:44 +0700

    tutorial 4 completed

--- main/views.py
+++ main/views.py
@@ -121,3 +121,21 @@ def create_news(request):
         return redirect('main:show_main')
     context = {'form': form}
     return render(request, "create_news.html", context)
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
